f7b0e36f901c3e1add0dc17d32904ec464c20f88
Surya Datta <sdatta@gotapco.com>
2020-03-02 15:41:04 +0530

    changed button side

--- js/dark.js
+++ js/dark.js
@@ -1,7 +1,7 @@
 var options = {
-  bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
+  bottom: '32px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
   time: '0.5s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff', // default: '#fff'
